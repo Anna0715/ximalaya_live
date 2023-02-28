6954ef3bd7a310bdd7269d55b2abdc6970ab2910
rou.zhang <rou.zhang@ximalaya.com>
2023-02-28 10:57:04 +0800

    去掉cookie逻辑

---
+++ frontend/src/locales/lang/zh-CN/menu.js
@@ -0,0 +1,13 @@
+export default {
+  'menu.welcome': '欢迎',
+  'menu.home': '主页',
+  'menu.consumer': 'C端通用工具',
+  'menu.consumer.certification': '开通实名认证',
+  'menu.consumer.video-permissions': '开通视频开播权限',
+  'menu.business': 'B端通用工具',
+  'menu.business.createBroadcast': '创建课程直播',
+  'menu.exception.not-find': '404',
+  'menu.exception.trigger': '触发错误',
+  'menu.account.trigger': '触发报错',
+  'menu.account.logout': '退出登录'
+}
---
+++ frontend/src/views/exception/404.vue
@@ -0,0 +1,20 @@
+<template>
+  <a-result status="404" title="网页暂未开发" >
+    <template #extra>
+      <a-button type="primary" @click="toHome">
+        回到首页
+      </a-button>
+    </template>
+  </a-result>
+</template>
+
+<script>
+export default {
+  name: 'Exception404',
+  methods: {
+    toHome () {
+      this.$router.push({ path: '/' })
+    }
+  }
+}
+</script>
