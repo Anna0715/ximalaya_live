2d0dbc227410c1336045890728b6a8f17c9ad50d
yeyi <yi.ye@trip.com>
2023-03-09 13:48:06 +0800

    fix: 接口域名更改

--- frontend/src/api/certification.js
+++ frontend/src/api/certification.js
@@ -4,28 +4,18 @@ import request from '@/utils/request'
 const domainName = 'http://192.168.114.36:7169/ximalive-qa'
 
 const api = {
-  addVprofileVerify: '/AddVprofileVerify',
-  createCourseLive: '/CreatecourseLive'
+  addVprofileVerify: '/AddVprofileVerify'
 }
+
 export default api
+
 export function addVprofileVerify (parameter) {
   return request({
     url: domainName + api.addVprofileVerify,
     method: 'POST',
     data: JSON.stringify(parameter),
     headers: {
-      'Content-Type': 'application/json;charset=UTF-8',
-    }
-  })
-}
-
-export function createCourseLive (parameter) {
-  return request({
-    url: domainName + api.createCourseLive,
-    method: 'POST',
-    data: JSON.stringify(parameter),
-    headers: {
-      'Content-Type': 'application/json;charset=UTF-8',
+      'Content-Type': 'application/json;charset=UTF-8'
     }
   })
 }
