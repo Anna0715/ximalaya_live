38e29109c8454b70672a99bef001ef5b68f218e6
07Anna <16621325482@163.com>
2023-04-10 16:29:11 +0800

    修改课程直播页面

--- frontend/src/api/certification.js
+++ frontend/src/api/certification.js
@@ -1,21 +1,31 @@
 import request from '@/utils/request'
 
 // const domainName = 'http://192.168.114.36:80'
-const domainName = 'http://192.168.114.36:7169/ximalive-qa'
-
+// const domainName = 'http://192.168.114.36:7169/ximalive-qa'
+const domainName = 'http://ops.test.ximalaya.com/ximalive-qa'
 const api = {
-  addVprofileVerify: '/AddVprofileVerify'
+  addVprofileVerify: '/AddVprofileVerify',
+  createCourseLive: '/CreatecourseLive'
 }
-
 export default api
-
 export function addVprofileVerify (parameter) {
   return request({
     url: domainName + api.addVprofileVerify,
     method: 'POST',
     data: JSON.stringify(parameter),
     headers: {
-      'Content-Type': 'application/json;charset=UTF-8'
+      'Content-Type': 'application/json;charset=UTF-8',
+    }
+  })
+}
+
+export function createCourseLive (parameter) {
+  return request({
+    url: domainName + api.createCourseLive,
+    method: 'POST',
+    data: JSON.stringify(parameter),
+    headers: {
+      'Content-Type': 'application/json;charset=UTF-8',
     }
   })
 }
